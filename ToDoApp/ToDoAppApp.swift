56a07ceaed50cd46413384dd44740e2e20348bd6
abdullaharslan01 <abdullaharslan1473@gmail.com>
2024-06-13 13:53:31 +0300

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by abdullah on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
